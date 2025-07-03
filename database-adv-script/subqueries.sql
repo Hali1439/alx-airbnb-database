74f960a59d31724a27bf147c0d6cd6936813cb3f
Halima Muktar <halimamuk1307@gmail.com>
2025-07-03 23:10:40 +0300

    Update subqueries.sql

--- database-adv-script/subqueries.sql
+++ database-adv-script/subqueries.sql
@@ -1,23 +1,28 @@
--- 1. Non-Correlated Subquery:
--- Find all properties where the average rating is greater than 4.0
+-- 1. Non-correlated subquery: Find all properties where the average rating is greater than 4.0
+SELECT
+    p.property_id,
+    p.property_name
+FROM
+    properties p
+WHERE
+    (
+        SELECT AVG(r.rating)
+        FROM reviews r
+        WHERE r.property_id = p.property_id
+    ) > 4.0;
+-- This subquery is non-correlated in the WHERE clause, referencing only the outer property_id.
 
-SELECT *
-FROM properties
-WHERE id IN (
-    SELECT property_id
-    FROM reviews
-    GROUP BY property_id
-    HAVING AVG(rating) > 4.0
-);
-
-
--- 2. Correlated Subquery:
--- Find users who have made more than 3 bookings
-
-SELECT *
-FROM users u
-WHERE (
-    SELECT COUNT(*)
-    FROM bookings b
-    WHERE b.user_id = u.id
-) > 3;
+-- 2. Correlated subquery: Find users who have made more than 3 bookings
+SELECT
+    u.user_id,
+    u.username,
+    u.email
+FROM
+    users u
+WHERE
+    (
+        SELECT COUNT(*)
+        FROM bookings b
+        WHERE b.user_id = u.user_id
+    ) > 3;
+-- This subquery is correlated, referencing each user row in the outer query.
